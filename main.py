7e0a7c70cc4d9abaf4ac94d9d3660640ebebfc66
Hector <hector.montes1@upr.edu>
2019-12-11 22:55:04 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from utilities import LanguageLexer, LanguageParser, FileReader
+from menu import ConsoleMenu
+from termcolor import colored
+
+
+#Run the program
+menu = ConsoleMenu()
+menu.run_menu()
+
